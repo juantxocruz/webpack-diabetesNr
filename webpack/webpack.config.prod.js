6108f170997b9416c71e05ed3959b1592ee325fd
Juantxo <jcruz16@gmail.com>
2025-01-27 19:05:30 +0100

    webpack config

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,8 +1,10 @@
+const path = require('path');
 const Webpack = require('webpack');
 const common = require('./webpack.common.js');
 const { merge } = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
 
 module.exports = merge(common, {
   mode: 'production',
